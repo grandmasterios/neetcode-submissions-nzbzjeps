9d3cea539ce2a39067764b1e71dde21bd45c67ea
Erkin <119873719+Melis-Baike@users.noreply.github.com>
2026-03-31 05:30:10 +1200

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    private int[][] directions = {
+        {0, 1}, // right
+        {0, -1}, // left
+        {1, 0}, // bottom
+        {-1, 0} // top
+    };
+
+    public void islandsAndTreasure(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                // Multi-Source
+                if(grid[i][j] == 0) {
+                    queue.add(new int[]{i, j});
+                }
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            int[] source = queue.poll();
+            int r = source[0];
+            int c = source[1];
+
+            for(int[] dir : directions) {
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 2147483647) {
+                    grid[nr][nc] = grid[r][c] + 1;
+                    queue.add(new int[]{nr, nc});
+                }
+            }
+        }
+    }
+}
